daba07c7fb0e91f4492f1c8c4b15b0cbde10237d
Elnaza <helzahggl@gmail.com>
2023-09-18 01:26:00 +0300

    Добавлена Задача27

---
+++ Zadacha27/Program.cs
@@ -0,0 +1,23 @@
+﻿//Ввести число и выдасть сумму всех чисел
+Console.WriteLine("Введите число ");
+
+if (!Int32.TryParse(Console.ReadLine(), out int number))
+{
+    Console.WriteLine("Вы ввели не число!");
+}
+
+int res = GetSumma( number);
+Console.WriteLine(res);
+
+int GetSumma(int number)
+{
+    int sum = 0;
+    char[] arr = number.ToString().ToCharArray ();
+
+    foreach (var s in arr)
+    {
+        Int32.TryParse(s.ToString(), out int res);
+        sum += res;
+    }
+    return sum;
+}
